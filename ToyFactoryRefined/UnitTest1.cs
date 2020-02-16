a9e2247c275a3fd2f3931438ff7d7933a114c805
Santosh Selvakumar <Santosh.Selvakumar@serko.com>
2020-02-17 11:08:43 +1300

    Add test for 2 square and 1 triangle

--- ToyFactoryRefined/UnitTest1.cs
+++ ToyFactoryRefined/UnitTest1.cs
@@ -10,15 +10,17 @@ namespace ToyFactoryRefined
         Shape shape;
         Shape redSquare;
         Shape blueSquare;
+        Shape redTriangle;
         //Dictionary<string, decimal> shapeCostDict; 
-        List<Shape> squareList; 
+        List<Shape> shapeList; 
         [SetUp]
         public void Setup()
         {
             shape = new Shape();
             redSquare = new Shape("Square","Red",1);
             blueSquare = new Shape("Square", "Blue", 1);
-            squareList = new List<Shape>();
+            redTriangle = new Shape("Triangle","Red", 2);
+            shapeList = new List<Shape>();
             //shapeCostDict = new Dictionary<string, decimal>();
             //shapeCostDict.Add("Square", 1);
             
@@ -27,27 +29,40 @@ namespace ToyFactoryRefined
         [Test]
         public void CalculateTotal_EmptyList_Return0()
         {
-            var total = Shape.CalculateTotal(squareList);
+            var total = Shape.CalculateTotal(shapeList);
             Assert.AreEqual(total,0);
         }
         [Test]
         public void CalculateTotal_SingleRedSquare_Return2()
         {
-            squareList.Add(redSquare);
+            shapeList.Add(redSquare);
             redSquare.ShapeCount = 1;
-            var total = Shape.CalculateTotal(squareList);
+            var total = Shape.CalculateTotal(shapeList);
             Assert.AreEqual(total, 2);
         }
 
         [Test]
         public void CalculateTotal_RedandBlueSquares_Return3()
         {
-            squareList.Add(redSquare);
-            squareList.Add(blueSquare);
+            shapeList.Add(redSquare);
+            shapeList.Add(blueSquare);
             redSquare.ShapeCount = 1;
             blueSquare.ShapeCount = 1;
-            var total = Shape.CalculateTotal(squareList);
+            var total = Shape.CalculateTotal(shapeList);
             Assert.AreEqual(total, 3);
+        }
+
+        [Test]
+        public void CalculateTotal_RedBlueSquaresandRedTriangle_Return6()
+        {
+            shapeList.Add(redSquare);
+            shapeList.Add(blueSquare);
+            shapeList.Add(redTriangle);
+            redTriangle.ShapeCount = 1;
+            redSquare.ShapeCount = 1;
+            blueSquare.ShapeCount = 1;
+            var total = Shape.CalculateTotal(shapeList);
+            Assert.AreEqual(total, 6);
         }
 
 
