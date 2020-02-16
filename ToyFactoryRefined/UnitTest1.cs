3e78137b073f9633aed1623835c7adaea1b71d0b
Santosh Selvakumar <Santosh.Selvakumar@serko.com>
2020-02-17 12:00:01 +1300

    Added  test for cirlce, square and triangle

--- ToyFactoryRefined/UnitTest1.cs
+++ ToyFactoryRefined/UnitTest1.cs
@@ -11,6 +11,7 @@ namespace ToyFactoryRefined
         Shape redSquare;
         Shape blueSquare;
         Shape redTriangle;
+        Shape redCircle;
         //Dictionary<string, decimal> shapeCostDict; 
         List<Shape> shapeList; 
         [SetUp]
@@ -19,6 +20,7 @@ namespace ToyFactoryRefined
             redSquare = new Shape("Square","Red",1);
             blueSquare = new Shape("Square", "Blue", 1);
             redTriangle = new Shape("Triangle","Red", 2);
+            redCircle = new Shape("Circle", "Red", 3);
             shapeList = new List<Shape>();
             //shapeCostDict = new Dictionary<string, decimal>();
             //shapeCostDict.Add("Square", 1);
@@ -62,6 +64,21 @@ namespace ToyFactoryRefined
             blueSquare.ShapeCount = 1;
             var total = Shape.CalculateTotal(shapeList);
             Assert.AreEqual(total, 6);
+        }
+
+        [Test]
+        public void CalculateTotal_RedBlueSquaresRedTriangleandRedCircle_Return14()
+        {
+            shapeList.Add(redSquare);
+            shapeList.Add(blueSquare);
+            shapeList.Add(redTriangle);
+            shapeList.Add(redCircle);
+            redTriangle.ShapeCount = 1;
+            redSquare.ShapeCount = 1;
+            blueSquare.ShapeCount = 1;
+            redCircle.ShapeCount = 2;
+            var total = Shape.CalculateTotal(shapeList);
+            Assert.AreEqual(total, 14);
         }
 
 
