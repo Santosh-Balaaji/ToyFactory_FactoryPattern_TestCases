be16102017f5e8d4d888716f2d85cdf1c75eef61
Santosh Selvakumar <Santosh.Selvakumar@serko.com>
2020-02-17 11:56:07 +1300

    Decoupled the tests and business logic

--- ToyFactory/Shape.cs
+++ ToyFactory/Shape.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ToyFactoryRefined
+namespace ToyFactory
 {
     public class Shape
     {
@@ -12,7 +12,7 @@ namespace ToyFactoryRefined
         public decimal Cost { get; set; }
 
         public Shape()
-        { 
+        {
         }
         public Shape(string shapeName, string color, decimal shapeCost)
         {
@@ -20,7 +20,8 @@ namespace ToyFactoryRefined
             Color = color;
             Cost = shapeCost;
         }
-        public static decimal CalculateTotal(List<Shape> shapeList) {
+        public static decimal CalculateTotal(List<Shape> shapeList)
+        {
             decimal total = 0;
 
             foreach (var shape in shapeList)
--- ToyFactoryRefined/UnitTest1.cs
+++ ToyFactoryRefined/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using ToyFactory;
 
 namespace ToyFactoryRefined
 {
