23686333e30c1863296049c40368f01de70c97fc
Santosh Selvakumar <Santosh.Selvakumar@serko.com>
2020-02-18 10:33:41 +1300

    added func to fetch shapelist from concretefactory class in consolebase class

--- ToyFactory/ConsoleBase.cs
+++ ToyFactory/ConsoleBase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ToyFactory.Factories;
 using ToyFactory.Reports;
 
 namespace ToyFactory
@@ -10,6 +11,9 @@ namespace ToyFactory
         public ConsoleBase()
         {
             var order = GetUserDetails();
+            order.ShapeList = GenerateShapeListFromFactory();
+
+
         }
 
         public static Order GetUserDetails()
@@ -28,6 +32,25 @@ namespace ToyFactory
             Console.WriteLine("Please input your Due Date");
             order.DueDate = Console.ReadLine();
             return order;
+        }
+
+        public static List<Shape> GenerateShapeListFromFactory()
+        {
+            var factories = GenerateFactoryParameters();
+            var shapeList = ConcreteShapeFactory.GenerateShapeList(factories);
+
+            return shapeList;
+        }
+
+        public static Dictionary<string, ShapeFactory> GenerateFactoryParameters()
+        {
+            var factories = new Dictionary<string, ShapeFactory>();
+            factories.Add("Square", new SquareFactory());
+            factories.Add("Circle", new CircleFactory());
+            factories.Add("Triangle", new TriangleFactory());
+
+            return factories;
+
         }
     }
 }
