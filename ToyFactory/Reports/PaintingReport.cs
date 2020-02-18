9d4090e773acef907a63d2575f6fcc56c2e457ba
Santosh Selvakumar <Santosh.Selvakumar@serko.com>
2020-02-18 14:20:45 +1300

    Refactored PaintingReport

--- ToyFactory/Reports/PaintingReport.cs
+++ ToyFactory/Reports/PaintingReport.cs
@@ -6,19 +6,34 @@ namespace ToyFactory.Reports
 {
     public class PaintingReport : IGenerateReport
     {
-        public void GenerateReport(Order order)
+        public static Order Order { get; set; }
+
+        public PaintingReport(Order order)
+        {
+            Order = order;
+        }
+        public void GenerateReport()
         {
             Console.WriteLine("Your painting report has been generated:");
-            Console.WriteLine("Name: {0} Address: {1} Due Date: {2} Order #: {3}", order.CustomerName, order.Address, order.DueDate, Order.OrderID);
+            DisplayUserDetails();
+            DisplayPaintingMatrix();
+        }
+        public static void DisplayUserDetails()
+        {
+            Console.WriteLine("Name: {0} Address: {1} Due Date: {2} Order #: {3}", Order.CustomerName, Order.Address, Order.DueDate, Order.OrderID);
+        }
+
+        public static void DisplayPaintingMatrix()
+        {
             Console.WriteLine("|        | {0}  | {1} | {2} |", Enum.GetName(typeof(EnumerationValues.Color), 0), Enum.GetName(typeof(EnumerationValues.Color), 1), Enum.GetName(typeof(EnumerationValues.Color), 2));
             Console.WriteLine("|--------|------|------|--------|");
             foreach (var shape in Enum.GetNames(typeof(EnumerationValues.Shapes)))
             {
                 Console.WriteLine("| {0} | {1}    | {2}    | {3}      |",
                     shape,
-                    Shape.GetTotalNoOfItemsPerShapeandColor(order.ShapeList, shape, Enum.GetName(typeof(EnumerationValues.Color), 0)),
-                    Shape.GetTotalNoOfItemsPerShapeandColor(order.ShapeList, shape, Enum.GetName(typeof(EnumerationValues.Color), 1)),
-                    Shape.GetTotalNoOfItemsPerShapeandColor(order.ShapeList, shape, Enum.GetName(typeof(EnumerationValues.Color), 2))
+                    Shape.GetTotalNoOfItemsPerShapeandColor(Order.ShapeList, shape, Enum.GetName(typeof(EnumerationValues.Color), 0)),
+                    Shape.GetTotalNoOfItemsPerShapeandColor(Order.ShapeList, shape, Enum.GetName(typeof(EnumerationValues.Color), 1)),
+                    Shape.GetTotalNoOfItemsPerShapeandColor(Order.ShapeList, shape, Enum.GetName(typeof(EnumerationValues.Color), 2))
                     );
 
             }
